8572c411a766737a30a184bb689730560d199ce2
Walt13 <66094305+Walt13@users.noreply.github.com>
2024-09-27 16:32:06 -0700

    Started making visualizations in Tableau. Still looking at start station data.

--- sql_queries/12_analyze_stations.sql
+++ sql_queries/12_analyze_stations.sql
@@ -16,3 +16,42 @@ ORDER BY
     count_station DESC;
 
 
+-- Top stations just for casual riders
+SELECT
+    start_station_name,
+    COUNT(start_station_name) AS count_station
+FROM
+    trips_year
+WHERE
+    start_station_name != 'Not At Station' AND
+    member_casual = 'casual'
+GROUP BY
+    start_station_name
+ORDER BY
+    count_station DESC
+LIMIT 20;
+
+
+SELECT
+    start_station_name,
+    start_lat,
+    start_long
+FROM
+    trips_year
+WHERE
+    start_station_name = 'Streeter Dr & Grand Ave'
+
+
+SELECT
+    start_station_name,
+    COUNT(start_station_name) AS count_station
+FROM
+    trips_year
+WHERE
+    start_station_name != 'Not At Station' AND
+    member_casual = 'member'
+GROUP BY
+    start_station_name
+ORDER BY
+    count_station DESC
+LIMIT 20;
